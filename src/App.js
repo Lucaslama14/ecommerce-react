f41bfd5cfc11c45d7aae1e30357a6b846508059d
Lucaslama14 <89704735+Lucaslama14@users.noreply.github.com>
2022-07-13 17:19:30 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Flor de ceramica</h1>
+      <nav class="menu">
+        <ul >
+          <li><a class="sesion" >Inicio</a></li>
+          <li><a class="sesion" >Contacto</a></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
